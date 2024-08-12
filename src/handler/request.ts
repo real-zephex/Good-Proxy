9c44ac187bd5c8d30fb5dae5b9d47153db69e833
real-zephex <zephex-1@outlook.com>
2024-08-12 20:21:59 +0530

    support for vtt (thumbnails) added

--- src/handler/request.ts
+++ src/handler/request.ts
@@ -18,11 +18,39 @@ export async function RequestHandler({ response }: { response: HonoRequest }) {
     }); // making request
 
     const type = fetchedRespone.headers.get("Content-Type") || "text/plain"; // detecting type of the response
-    const responseBody: BodyInit | null = fetchedRespone.body; // getting content
+    let responseBody;
+    // THIS LITERALLY TOOK 5 HOURS
+    if (type.includes("text/vtt")) {
+      console.log("VTT file found");
+      responseBody = (await fetchedRespone.text()) as string;
 
-    // M3U8 Check
+      const regex = /.+?\.(jpg)+/g;
+      const matches = [...responseBody.matchAll(regex)];
 
+      let fileNames: string[] = [];
+      // Iterate over matches
+      for (const match of matches) {
+        const filename = match[0];
+        if (!fileNames.includes(filename)) {
+          fileNames.push(filename);
+        }
+      }
+
+      if (fileNames.length > 0) {
+        for (const filename of fileNames) {
+          const newUrl = url.replace(/\/[^\/]*$/, `/${filename}`);
+          responseBody = responseBody.replaceAll(
+            filename,
+            "https://goodproxy.zephex0-f6c.workers.dev/fetch?url=" + newUrl
+          );
+        }
+      }
+    } else {
+      console.log(type);
+      responseBody = fetchedRespone.body;
+    }
     corsHeaders["Content-Type"] = type;
+
     return new Response(responseBody, {
       headers: corsHeaders,
       status: fetchedRespone.status,
